0f576788a81ec8edde4462e18320c03fd5f2cf5e
damjanh <damjan.hudobreznik@gmail.com>
2020-06-30 13:51:21 +0200

    Implement combat log.

--- app.js
+++ app.js
@@ -4,37 +4,55 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: [],
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            const damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for:' + damage, 
+            });
             if (this.checkWin()) {
                 return;
             };
             this.monsterAttacks();
         },
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            const damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster hard for:' + damage, 
+            });
             if (this.checkWin()) {
                 return;
             };
             this.monsterAttacks();
         },
         heal: function () {
-            if (this.playerHealth <= 90) {
-                this.playerHealth += 10;
+            const heal = 10;
+            if (this.playerHealth <= 100 - heal) {
+                this.playerHealth += heal;
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for:' + heal, 
+            });
             this.monsterAttacks();
         },
         giveUp: function() {
             this.gameIsRunning = false;
+            this.turns = [];
         },
         calculateDamage: function(min, max) {
            return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -58,8 +76,13 @@ new Vue({
             return false;
         },
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5,12);
+            const damage = this.calculateDamage(5,12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for:' + damage, 
+            });
             this.checkWin();
-        }
+        },
     }
 });
